2beb7e3247047c3e7ad33f326fd8f0ffc516eb9b
Deepak <darkdeepak001@gmail.com>
2026-02-02 22:15:55 +0530

    Add company login validation system

---
+++ Login Based Problems /company_login_validation_system.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.System;
+class problem16
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your age :");
+        int age = scan.nextInt();
+        System.out.print("Enter 0 if you have i'd or 1 if you don't have i'd");
+        int id = scan.nextInt();
+        System.out.print("Enter your experience : ");
+        int experience = scan.nextInt();
+        String result = (id==1) ? "access denied" : (experience>=5 && age>=25) ? "Admin access " : (experience>=2) ? "User access" : "limited access";
+        System.out.println("You access level is : "+result); 
+    }
+}
