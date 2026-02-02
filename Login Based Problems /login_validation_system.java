bb277036a62938d2da7cfc417691ccab7a393bda
Deepak <darkdeepak001@gmail.com>
2026-02-02 22:01:23 +0530

    Implement login validation system in Java

---
+++ Login Based Problems /login_validation_system.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.System;
+class problem7
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        String user_name="Deepak@123";
+        String password="Deepak2911";
+        int otp=493439;
+        System.out.print("Enter your User name : ");
+        String u_name=scan.nextLine();
+        if(user_name.equals(u_name))
+        {
+            System.out.print("Enter your Password : ");
+            String pass=scan.nextLine();
+            if(password.equals(pass))
+            {
+                System.out.print("Enter otp : ");
+                int o=scan.nextInt();
+                if(otp==o)
+                {
+                    System.out.print("Login Successfully!!!");
+                }
+                else
+                {
+                    System.out.print("Incorrect OPT is Entered!!!");
+                }
+
+            }
+            else
+            {
+            System.out.print("Incorrect Password was entered!!!");
+            }
+        }
+        else
+        {
+            System.out.print("Incorrect Username  was entered!!!");
+        }
+}   }
