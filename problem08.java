01703f810b0ed6a85695e20abfac6e2acffcdd4a
Deepak <darkdeepak001@gmail.com>
2026-02-03 19:46:39 +0530

    Add practice class to print a 3x3 star pattern

---
+++ problem08.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class practice
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
