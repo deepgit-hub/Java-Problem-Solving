2857f05977d88f33abd7d4f172adeda9d628e5f8
Deepak <darkdeepak001@gmail.com>
2026-02-03 19:40:37 +0530

    Add multiplication table generator in problem07.java

---
+++ problem07.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class practice
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=100000;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                System.out.println(i+"*"+j+" = "+(i*j));
+            }
+            System.out.println();
+        }
+    }
+}
