6fa21315c2a94930b0701fff9f99cb576041d91e
Deepak <darkdeepak001@gmail.com>
2026-02-07 22:55:05 +0530

    Create problem21.java

---
+++ simple_practiced_problems/problem21.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+class problem21
+{
+  static void add(int a,int b)
+  {
+    int sum = a+b;
+    System.out.print("The Addition  is : "+sum);
+  }
+  public static void main(String args[])
+  {
+    add(453,546);
+  }
+}
