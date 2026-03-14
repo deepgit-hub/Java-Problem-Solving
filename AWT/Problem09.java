d4bbfc25aef1e95b617db568c7c3b5c90060531f
Deepak <darkdeepak001@gmail.com>
2026-03-14 18:11:12 +0530

    Update Problem09.java

--- AWT/Problem09.java
+++ AWT/Problem09.java
@@ -1,15 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 class P
 {
     P()
     {
-        JFrame f = new Frame("Registration Form");
+        JFrame f = new JFrame("Registration Form");
         f.setLayout(null);
 
         JLabel name = new JLabel("Name : ");
         name.setBounds(50,30,100,30);
-        JTextField nameinput = new JTextField("Enter your Name");
+        JTextField nameinput = new JTextField("Enter Your Name");
         nameinput.setBounds(180,30,270,30);
 
         JLabel registration_number = new JLabel("Register Number : ");
@@ -17,65 +18,58 @@ class P
         JTextField registration_numberinput = new JTextField("Enter your Registration Number");
         registration_numberinput.setBounds(180,60,270,30);
 
-        JLabel gender = new Label("Gender : ");
+        JLabel gender = new JLabel("Gender : ");
         gender.setBounds(50,90,100,30);
-        JChoice choosegender = new Choice();
-        choosegender.add("Male");
-        choosegender.add("Female");
-        choosegender.setBounds(180,90,270,30);
 
-        JLabel department = new Label("Department : ");
+        JRadioButton male = new JRadioButton("Male");
+        JRadioButton female = new JRadioButton("FeMale");
+        male.setBounds(180,90,100,30);
+        female.setBounds(290,90,100,30);
+
+        ButtonGroup g = new ButtonGroup();
+        g.add(male);
+        g.add(female);
+
+        JLabel department = new JLabel("Department : ");
         department.setBounds(50,120,100,30);
-        JChoice choosedepartment = new Choice();
-        choosedepartment.add("CS");
-        choosedepartment.add("BCA");
-        choosedepartment.add("MCA");
+        
+        String d[] ={"CS","BCA","MCA"};
+        JComboBox choosedepartment = new JComboBox(d);
         choosedepartment.setBounds(180,120,270,30);
 
-        JLabel course = new Label("Course : ");
+        JLabel course = new JLabel("Course : ");
         course.setBounds(50,150,100,30);
-        JChoice choosecourse = new Choice();
-        choosecourse.add("UG");
-        choosecourse.add("PG");
+        
+        String c[]={"UG","PG"};
+        JComboBox choosecourse = new JComboBox(c);
         choosecourse.setBounds(180,150,270,30);
 
-        Label dob= new Label("Date of Birth : ");
+        JLabel dob= new JLabel("Date of Birth : ");
         dob.setBounds(50,180,100,30);
-        TextField dateofbirth = new TextField("Enter Your Date of Birth");
+        JTextField dateofbirth = new JTextField("Enter Your Date of Birth");
         dateofbirth.setBounds(180,180,270,30);
 
-        Label age = new Label("Age : ");
+        JLabel age = new JLabel("Age : ");
         age.setBounds(50,210,100,30);
-        TextField enterage = new TextField("Enter Your Age");
+        JTextField enterage = new JTextField("Enter Your Age");
         enterage.setBounds(180,210,270,30);
         
-        Label language = new Label("Languages Known : ");
+        JLabel language = new JLabel("Languages Known : ");
         language.setBounds(50,240,100,30);
-        Checkbox language1 = new Checkbox("C");
-        Checkbox language2 = new Checkbox("C++");
-        Checkbox language3 = new Checkbox("JAVA");
-        Checkbox language4 = new Checkbox("Python");
+        JCheckBox language1 = new JCheckBox("C");
+        JCheckBox language2 = new JCheckBox("C++");
+        JCheckBox language3 = new JCheckBox("JAVA");
+        JCheckBox language4 = new JCheckBox("Python");
         language1.setBounds(180,240,50,30);
         language2.setBounds(240,240,50,30);
         language3.setBounds(300,240,50,30);
         language4.setBounds(360,240,70,30);
-        
-        Button summit = new Button("Summit");
-        summit.setBounds(200,300,70,30);
-        ActionListener summitlistener = new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                System.out.println(nameinput.getText());
-                System.out.println(registration_numberinput.getText());
-                System.out.println(dateofbirth.getText());
-                System.out.println(enterage.getText());
-            }
-        };
-        summit.addActionListener(summitlistener);
+
         f.add(name);
         f.add(registration_number);
         f.add(gender);
+        f.add(male);
+        f.add(female);
         f.add(department);
         f.add(course);
         f.add(dob);
@@ -83,7 +77,6 @@ class P
         f.add(language);
         f.add(nameinput);
         f.add(registration_numberinput);
-        f.add(choosegender);
         f.add(choosedepartment);
         f.add(choosecourse);
         f.add(dateofbirth);
@@ -92,11 +85,8 @@ class P
         f.add(language2);
         f.add(language3);
         f.add(language4);
-        f.add(summit);
         f.setVisible(true);
         f.setSize(500,500);
-
-
     }
     public static void main(String args[])
     {
