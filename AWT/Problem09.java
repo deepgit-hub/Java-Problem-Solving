807356b1bdfcde69d5525228ca0e3183e95d90e6
Deepak <darkdeepak001@gmail.com>
2026-03-14 17:03:58 +0530

    Update Problem09.java

--- AWT/Problem09.java
+++ AWT/Problem09.java
@@ -1,34 +1,78 @@
 import java.awt.*;
+import java.awt.event.*;
 class P
 {
     P()
     {
-        Frame f = new Frame("Registration Form");
+        JFrame f = new Frame("Registration Form");
         f.setLayout(null);
-        Label name = new Label("Name : ");
+
+        JLabel name = new JLabel("Name : ");
         name.setBounds(50,30,100,30);
-        TextField nameinput = new TextField("Enter your Name");
+        JTextField nameinput = new JTextField("Enter your Name");
         nameinput.setBounds(180,30,270,30);
-        Label registration_number = new Label("Register Number : ");
+
+        JLabel registration_number = new JLabel("Register Number : ");
         registration_number.setBounds(50,60,110,30);
-        TextField registration_numberinput = new TextField("Enter your Registration Number");
+        JTextField registration_numberinput = new JTextField("Enter your Registration Number");
         registration_numberinput.setBounds(180,60,270,30);
-        Label gender = new Label("Gender : ");
+
+        JLabel gender = new Label("Gender : ");
         gender.setBounds(50,90,100,30);
-        Choice choosegender = new Choice();
+        JChoice choosegender = new Choice();
         choosegender.add("Male");
         choosegender.add("Female");
-        choosegender.setBounds(180,90,150,30);
-        Label department = new Label("Department : ");
+        choosegender.setBounds(180,90,270,30);
+
+        JLabel department = new Label("Department : ");
         department.setBounds(50,120,100,30);
-        Label course = new Label("Course : ");
+        JChoice choosedepartment = new Choice();
+        choosedepartment.add("CS");
+        choosedepartment.add("BCA");
+        choosedepartment.add("MCA");
+        choosedepartment.setBounds(180,120,270,30);
+
+        JLabel course = new Label("Course : ");
         course.setBounds(50,150,100,30);
+        JChoice choosecourse = new Choice();
+        choosecourse.add("UG");
+        choosecourse.add("PG");
+        choosecourse.setBounds(180,150,270,30);
+
         Label dob= new Label("Date of Birth : ");
         dob.setBounds(50,180,100,30);
+        TextField dateofbirth = new TextField("Enter Your Date of Birth");
+        dateofbirth.setBounds(180,180,270,30);
+
         Label age = new Label("Age : ");
         age.setBounds(50,210,100,30);
+        TextField enterage = new TextField("Enter Your Age");
+        enterage.setBounds(180,210,270,30);
+        
         Label language = new Label("Languages Known : ");
         language.setBounds(50,240,100,30);
+        Checkbox language1 = new Checkbox("C");
+        Checkbox language2 = new Checkbox("C++");
+        Checkbox language3 = new Checkbox("JAVA");
+        Checkbox language4 = new Checkbox("Python");
+        language1.setBounds(180,240,50,30);
+        language2.setBounds(240,240,50,30);
+        language3.setBounds(300,240,50,30);
+        language4.setBounds(360,240,70,30);
+        
+        Button summit = new Button("Summit");
+        summit.setBounds(200,300,70,30);
+        ActionListener summitlistener = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println(nameinput.getText());
+                System.out.println(registration_numberinput.getText());
+                System.out.println(dateofbirth.getText());
+                System.out.println(enterage.getText());
+            }
+        };
+        summit.addActionListener(summitlistener);
         f.add(name);
         f.add(registration_number);
         f.add(gender);
@@ -40,8 +84,17 @@ class P
         f.add(nameinput);
         f.add(registration_numberinput);
         f.add(choosegender);
+        f.add(choosedepartment);
+        f.add(choosecourse);
+        f.add(dateofbirth);
+        f.add(enterage);
+        f.add(language1);
+        f.add(language2);
+        f.add(language3);
+        f.add(language4);
+        f.add(summit);
         f.setVisible(true);
-        f.setSize(200,200);
+        f.setSize(500,500);
 
 
     }
