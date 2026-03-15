177bf7ede861c705b8a58ede6023a2e4c5f67692
Deepak <darkdeepak001@gmail.com>
2026-03-15 17:32:05 +0530

    Update Problem11.java

--- AWT/Problem11.java
+++ AWT/Problem11.java
@@ -3,14 +3,17 @@ import java.awt.event.*;
 class P
 {
     
-    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,plus,minus,divide,mul;
+    Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,plus,minus,divide,mul,equal;
+    TextField display;
+    String num1="", op="";
   
     P()
     {
         Frame f = new Frame();
-        f.setDefaultCloseOperation(Frame.EXIT_ON_CLOSE);
         f.setLayout(null);
-        TextField display = new TextField("0");
+        display = new TextField("0");
+        display.setBounds(200,170,100,30);
+        
         b0 = new Button("0");
         b0.setBounds(50,50,30,30);
 
@@ -52,7 +55,160 @@ class P
         mul.setBounds(50,170,30,30);
 
         divide = new Button("/");
-        mul.setBounds(80,170,30,30);
+        divide.setBounds(80,170,30,30);
+
+        equal = new Button("=");
+        equal.setBounds(100,200,30,30);
+
+        b0.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"0");
+            }
+        });
+
+        b1.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"1");
+            }
+        });
+
+        b2.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"2");
+            }
+        });
+
+        b3.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"3");
+            }
+        });
+
+        b4.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"4");
+            }
+        });
+
+        b5.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"5");
+            }
+        });
+
+
+        b6.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"6");
+            }
+        });
+
+
+        b7.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"7");
+            }
+        });
+
+        b8.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"8");
+            }
+        });
+
+        b9.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                display.setText(display.getText()+"9");
+            }
+        });
+
+        plus.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                num1=display.getText();
+                op="+";
+                display.setText("");
+            }
+        });
+
+        minus.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                num1=display.getText();
+                op="-";
+                display.setText("");
+            }
+        });
+
+        mul.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                num1=display.getText();
+                op="*";
+                display.setText("");
+            }
+        });
+
+        divide.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                num1=display.getText();
+                op="/";
+                display.setText("");
+            }
+        });
+
+        equal.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+            String num2 = display.getText();
+            int a = Integer.parseInt(num1);
+            int b = Integer.parseInt(num2);
+            int result =0;
+            if(op.equals("+"))
+            {
+                result= a+b;
+            }
+            if(op.equals("-"))
+            {
+                result = a-b;
+            }
+            if(op.equals("*"))
+            {
+                result = a*b;
+            }
+            if(op.equals("/"))
+            {
+                result = a/b;
+            }
+            display.setText(""+result);
+            }
+        });
 
         f.add(b0);
         f.add(b1);
@@ -69,6 +225,7 @@ class P
         f.add(mul);
         f.add(divide);
         f.add(display);
+        f.add(equal);
         f.setSize(700,700);
         f.setVisible(true);
         f.setTitle("Calculator");
