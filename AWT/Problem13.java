d7649df5bb3367aa51e772ecdbb9c66c1b30b9ee
Deepak <darkdeepak001@gmail.com>
2026-03-15 19:44:53 +0530

    Update Problem13.java

--- AWT/Problem13.java
+++ AWT/Problem13.java
@@ -7,6 +7,7 @@ class P
         Frame f = new Frame("Mouse Event Analyzer");
         f.setLayout(null);
         Label msg = new Label("Mouse not Entered");
+        msg.setBounds(100,400,200,30);
         f.addMouseListener(new MouseAdapter()
         {
             public void mouseEntered(MouseEvent e)
@@ -25,10 +26,19 @@ class P
             {
                 msg.setText("Mouse Released");
             }
+            public void mouseExited(MouseEvent e)
+            {
+                msg.setText("Mouse Exited");
+            }
         });
         f.setVisible(true);
         f.setSize(500,500);
-
+        f.add(msg);
+        }
+        public static void main(String args[])
+        {
+            new P();
+        }
         
-    }
+    
 }
